18329220caef96528483cbfd0eebf41b18e164eb
linga619 <lingadurai619@gmail.com>
2022-04-20 13:17:41 +0530

    new code

--- src/test/java/org/git/Git.java
+++ src/test/java/org/git/Git.java
@@ -4,6 +4,6 @@ public class Git {
 public static void main(String[] args) {
 	System.out.println("Selenium");
 	System.out.println("java");
-	System.out.println("maven");
+	System.out.println("testngggggg");
 }
 }
