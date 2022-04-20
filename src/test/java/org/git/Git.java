334f1dec44e70a98e40dc39c9887a6cd18dc6075
linga619 <lingadurai619@gmail.com>
2022-04-20 12:37:29 +0530

    project structure

--- src/test/java/org/git/Git.java
+++ src/test/java/org/git/Git.java
@@ -4,6 +4,6 @@ public class Git {
 public static void main(String[] args) {
 	System.out.println("Selenium");
 	System.out.println("testng");
-	System.out.println("maven");
+	System.out.println("junit");
 }
 }
